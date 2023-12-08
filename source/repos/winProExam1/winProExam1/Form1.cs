3fd1ef3053facbbc8e0ff93fa9296e5884c03f2a
yjg_wku <145737192+yjgwku@users.noreply.github.com>
2023-12-08 11:30:44 +0900

    로드 및 픽쳐박스,텍스트박스 코드 추가

--- source/repos/winProExam1/winProExam1/Form1.cs
+++ source/repos/winProExam1/winProExam1/Form1.cs
@@ -25,6 +25,30 @@ namespace 프로젝트
             InitializeComponent();
         }
 
+ 
+        private void pictureBox3_MouseDown(object sender, MouseEventArgs e)
+        {
+            textBox2.UseSystemPasswordChar = false;
+        }
 
+        private void pictureBox3_MouseUp(object sender, MouseEventArgs e)
+        {
+            textBox2.UseSystemPasswordChar = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = textBox1;
+        }
+
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 showform2 = new Form2();
+            showform2.Show();
+            
+
+        }
     }
 }
