64c2c5d04583810aa22f31ed4e5324672b506a84
yjg_wku <145737192+yjgwku@users.noreply.github.com>
2023-12-08 11:26:28 +0900

    부가파일 추가

---
+++ source/repos/winProExam1/winProExam1/Form4.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace scheduler
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Coltime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colmon = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.coltue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colwen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colthu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colfri = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.Disable;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Coltime,
+            this.colmon,
+            this.coltue,
+            this.colwen,
+            this.colthu,
+            this.colfri});
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.dataGridView1.GridColor = System.Drawing.Color.White;
+            this.dataGridView1.Location = new System.Drawing.Point(37, 207);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 50;
+            this.dataGridView1.Size = new System.Drawing.Size(1005, 522);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Coltime
+            // 
+            this.Coltime.HeaderText = "시간";
+            this.Coltime.MinimumWidth = 6;
+            this.Coltime.Name = "Coltime";
+            // 
+            // colmon
+            // 
+            this.colmon.HeaderText = "월요일";
+            this.colmon.MinimumWidth = 6;
+            this.colmon.Name = "colmon";
+            // 
+            // coltue
+            // 
+            this.coltue.HeaderText = "화요일";
+            this.coltue.MinimumWidth = 6;
+            this.coltue.Name = "coltue";
+            this.coltue.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // colwen
+            // 
+            this.colwen.HeaderText = "수요일";
+            this.colwen.MinimumWidth = 6;
+            this.colwen.Name = "colwen";
+            // 
+            // colthu
+            // 
+            this.colthu.HeaderText = "목요일";
+            this.colthu.MinimumWidth = 6;
+            this.colthu.Name = "colthu";
+            // 
+            // colfri
+            // 
+            this.colfri.HeaderText = "금요일";
+            this.colfri.MinimumWidth = 6;
+            this.colfri.Name = "colfri";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "월요일",
+            "화요일",
+            "수요일",
+            "목요일",
+            "금요일"});
+            this.comboBox1.Location = new System.Drawing.Point(6, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(85, 20);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(132, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "추가";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(40, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(213, 119);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "직접추가";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1교시(9시~10시)",
+            "2교시(10시~11시)",
+            "3교시(11시~12시)",
+            "4교시(12시~13시)",
+            "5교시(13시~14시)",
+            "6교시(14시~15시)",
+            "7교시(15시~16시)",
+            "8교시(16시~17시)",
+            "9교시(17시~18시)"});
+            this.comboBox2.Location = new System.Drawing.Point(107, 30);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(77, 20);
+            this.comboBox2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "시간";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "요일";
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1113, 755);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Coltime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colmon;
+        private System.Windows.Forms.DataGridViewTextBoxColumn coltue;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colwen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colthu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colfri;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
