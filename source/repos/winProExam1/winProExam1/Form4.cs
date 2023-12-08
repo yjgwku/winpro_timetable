ae6c56f4765551d48eaf4c4da664bf194a571a77
yjg_wku <145737192+yjgwku@users.noreply.github.com>
2023-12-08 11:40:37 +0900

    dataGridView 기능 삭제

--- source/repos/winProExam1/winProExam1/Form4.cs
+++ source/repos/winProExam1/winProExam1/Form4.cs
@@ -137,10 +137,7 @@ namespace scheduler
             }
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
 
-        }
 
         private void button1_Click(object sender, EventArgs e)
         {
