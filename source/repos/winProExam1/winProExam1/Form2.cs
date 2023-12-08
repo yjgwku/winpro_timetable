1737bfe8bcfa005ecd46969df8ec7de481658567
yjg_wku <145737192+yjgwku@users.noreply.github.com>
2023-12-08 11:38:15 +0900

    필요없는 코드 삭제

--- source/repos/winProExam1/winProExam1/Form2.cs
+++ source/repos/winProExam1/winProExam1/Form2.cs
@@ -52,21 +52,6 @@ namespace 프로젝트
             this.ActiveControl = textBox1;
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
 
-        }
-
-      
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
